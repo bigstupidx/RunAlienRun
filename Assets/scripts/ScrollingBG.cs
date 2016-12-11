ee4a290642a56fd78c796b668be4ec96303e9076
DESKTOP-I9QHGHT\Nasser <n.metanii@gmail.com>
2016-12-12 01:58:31 +0400

    A title for the main menu , added scrolling BG and some edits

---
+++ Assets/scripts/ScrollingBG.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBG : MonoBehaviour {
+
+    public float speed = 0.1f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 offest = new Vector2(Time.time * speed, 0);
+
+        GetComponent<Renderer>().material.mainTextureOffset = offest;
+	}
+}
